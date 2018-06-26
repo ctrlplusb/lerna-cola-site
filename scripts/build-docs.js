77c04f1b6f30bda77c96d0493cc153bc524cac44
Sean Matheson <sean@ctrlplusb.com>
2018-06-26 15:51:33 +0100

    Generalised file tree builder.

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,60 +1,22 @@
+/* eslint-disable no-console */
+
 const dedent = require('dedent')
-const fs = require('fs-extra')
 const path = require('path')
-const capitalize = require('lodash.capitalize')
-const rimraf = require('rimraf')
-const menu = require('../data/menu.json')
-const slugify = require('../utils/slugify')
+const fileTree = require('../utils/file-tree')
 
-const inputDir = path.resolve(process.cwd(), './data')
-const outputDir = path.resolve(process.cwd(), './pages/docs')
+fileTree.generate({
+  inputDir: path.resolve(process.cwd(), './data'),
+  outputDir: path.resolve(process.cwd(), './pages/docs'),
+  template: ({ depth, source }) =>
+    dedent(`
+  import React from 'react'
+  import Markdown from '../../${Array.from(new Array(depth))
+    .map(() => '../')
+    .join('')}components/markdown'
 
-rimraf.sync(outputDir)
+  const source = \`
+  ${source.replace(/`/gi, '\\\\`')}
+  \`
 
-fs.ensureDirSync(inputDir)
-fs.ensureDirSync(outputDir)
-
-const buildComponent = ({ depth, source }) =>
-  dedent(`
-    import React from 'react'
-    import Markdown from '../../${Array.from(new Array(depth))
-      .map(() => '../')
-      .join()}components/markdown'
-
-    const source = \`
-    ${source.replace(/`/gi, '\\\\`')}
-    \`
-
-    export default () => <Markdown source={source} />
-  `)
-
-buildComponent({
-  name: capitalize('Foo'),
-  source: '## Foo',
+  export default () => <Markdown source={source} />`),
 })
-
-const parseMenu = (current, prevDepth, prevPath) => {
-  const depth = prevDepth + 1
-  if (current.sections) {
-    current.sections.forEach(section => {
-      const sectionPath = path.join(prevPath, slugify(section.title))
-      fs.ensureDirSync(sectionPath)
-      parseMenu(section, depth, sectionPath)
-    })
-  }
-  if (current.entries) {
-    current.entries.forEach(entry => {
-      const filename = `${slugify(entry.title)}.js`
-      const source = fs.readFileSync(path.join(inputDir, entry.source), {
-        encoding: 'utf8',
-      })
-      fs.writeFileSync(
-        path.join(prevPath, filename),
-        buildComponent({ depth, source }),
-        { encoding: 'utf8' },
-      )
-    })
-  }
-}
-
-parseMenu(menu, -1, outputDir)
